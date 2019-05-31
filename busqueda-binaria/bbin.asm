82f7298a3d3a5d8afdf7e28e00c250a495c46955
Óscar <oxcabe@gmail.com>
2019-05-31 18:35:57 +0100

    Finish binary search proof of concept

--- busqueda-binaria/bbin.asm
+++ busqueda-binaria/bbin.asm
@@ -1,45 +1,57 @@
-21
-    ADDI R32 R0 #-1
-    ADDI R33 R0 #1 
+26
+	ADDI R32 R0 #-1
+	ADDI R33 R0 #1 
 
 // CONTAR 
 CONTAR:
-    LW R31 0(R30)
-    ADDI R30 R30 #2
-    BNE R31 R32 CONTAR
-    
-    // GUARDAR EN R30 N ELEMENTOS
-    ADDI R30 R30 #-2
-    SRLV R30 R30 R33
-    ADDI R30 R30 #-1
+	LW R31 0(R30)
+	ADDI R30 R30 #2
+	BNE R31 R32 CONTAR
+
+	// GUARDAR EN R30 N ELEMENTOS
+	ADDI R30 R30 #-2
+	SRLV R30 R30 R33
+	ADDI R30 R30 #-1
 
 // R35: i (tope izquierdo)
 // R30: n (tope derecho)
 // R34: v (sensor a buscar)
 // R36: p (posición del elemento)
 // R37: elemento
+// R38: 2i
 
-    LW R34 40(R0) // Cogemos sensor a buscar (en este caso, 4)
+	LW R34 40(R0) // Cogemos sensor a buscar (en este caso, 4)
 
 BUSQUEDA:
+	// p = (n - i + 1) / 2 * 2 + (2 * i)
+	SUB R36 R30 R35
+	ADDI R36 R36 #1
+	SRLV R36 R36 R33
+	SLLV R36 R36 R33
 
-    BEQ R30 R35 FINISH
-    
+	SLLV R38 R35 R33
+	ADD R36 R36 R38
 
-    // Cogemos elemento[p]
-    LW R37 0(R36)
+	// Terminar si i == n
+	BEQ R30 R35 FINISH
 
-    // if (v R34 < elemento R37) {n = p - 1}
-    // else {i = p + 1}
+	// Cogemos elemento[p]
+	LW R37 0(R36)
 
-    BGT R37 R34 IFGREATER
+	// Terminar si v == elemento[p]
+	BEQ R34 R37 FINISH
 
-    ADDI R30 R36 #-1
-    BEQ R0 R0 BUSQUEDA
+	// Condicionales entre v y el elemento en p
+	BGT R34 R37 IFGREATER
+
+	SRLV R30 R36 R33
+	ADDI R30 R30 #-1
+	BEQ R0 R0 BUSQUEDA
 
 IFGREATER:
-    ADDI R35 R36 #-1
-    BEQ R0 R0 BUSQUEDA
+	SRLV R35 R36 R33
+	ADDI R35 R35 #1
+	BEQ R0 R0 BUSQUEDA
 
 FINISH:
-    LW R40 0(R30)
+	LF F3 1(R36)
